194893ff7995e262cd82a645b31132d92ddf9401
Shanen <co1627@yahoo.com>
2019-08-29 20:49:09 +0000

    change html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "I finally figured it out!!"
+    render html: "I finally figured out the first part"
   end
 end
